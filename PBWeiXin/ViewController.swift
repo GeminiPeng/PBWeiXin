e9d4841ca232f100685dcf2addada212793da301
abcdpengbo123 <18321507001@163.com>
2017-01-06 17:49:53 +0800

    Initial Commit

---
+++ PBWeiXin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PBWeiXin
+//
+//  Created by Pengbo on 2017/1/6.
+//  Copyright © 2017年 swift小微信. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
